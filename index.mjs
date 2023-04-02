3e8a6622f6719d94798316a165bd92be59bdfa3a
Anthony <anthony.yc.ku@gmail.com>
2023-04-01 20:15:34 -0400

    first

--- index.mjs
+++ index.mjs
@@ -5,11 +5,11 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'https://pichael.org',
-    optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'https://pichael.org',
+//     optionsSuccessStatus: 200,
+// };
+app.use(cors());
 
 const apiKey = 'BA3825EFCD35B8CF4F48E58C9216E034';
 
@@ -23,6 +23,13 @@ app.get('/api/resolveVanityUrl/:vanityUrl', async (req, res) => {
 app.get('/api/getPlayerSummaries/:steamId', async (req, res) => {
     const steamId = req.params.steamId;
     const response = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${apiKey}&steamids=${steamId}`);
+    const data = await response.json();
+    res.json(data);
+});
+
+app.get('/api/getOwnedGames/:steamId', async (req, res) => {
+    const steamId = req.params.steamId;
+    const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`);
     const data = await response.json();
     res.json(data);
 });
